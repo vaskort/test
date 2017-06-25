966b67bf0ccfac7b345891362300de7f20745c52
Vasilis Kortsimelidis <vaskort@gmail.com>
2017-06-25 16:07:28 +0100

    the tab items work correctly and show the appropriate match or no match view

--- app/components/MatchViewWrapper.jsx
+++ app/components/MatchViewWrapper.jsx
@@ -7,14 +7,46 @@ import MatchView from "MatchView";
 import NonMatchView from "NonMatchView";
 
 class MatchViewWrapper extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tabShown: marketsData.marketData.markets.name,
+      firstClicked: false
+    };
+  }
+
+  componentDidMount() {
+    // lets find the first object and set the name to tabShown property so we have an active item
+    for (var key in marketsData.marketData.markets) {
+        console.log(marketsData.marketData.markets[key].name);
+        i++;
+    }
+  }
+
+  handleChange(e) {
+    this.setState({
+      tabShown: e.target.dataset.name,
+      firstClicked: true
+    });
+  }
+
   render() {
+    console.log(this.state);
     let bets = [];
     let betViews = [];
     let i = 0;
     for (var key in marketsData.marketData.markets) {
       // add to the best array the tab items
       bets.push(
-        <li key={i} className={classNames({ active: i === 0 })}>
+        <li
+          key={i}
+          data-name={marketsData.marketData.markets[key].name}
+          onClick={this.handleChange.bind(this)}
+          className={classNames({
+            active:
+              this.state.tabShown === marketsData.marketData.markets[key].name || i === 0 && this.state.firstClicked === false
+          })}
+        >
           {marketsData.marketData.markets[key].name}
         </li>
       );
@@ -22,10 +54,26 @@ class MatchViewWrapper extends Component {
       betViews.push(
         // depending on the type show the appropriate view
         marketsData.marketData.markets[key].type === "match"
-          ? <li key={i} className={classNames({ active: i === 0 })}>
+          ? <li
+              key={i}
+              className={classNames({
+                active:
+                  this.state.tabShown ===
+                    marketsData.marketData.markets[key].name || i === 0 && this.state.firstClicked === false
+              })}
+            >
               <MatchView bets={marketsData.marketData.markets[key].bets} />
             </li>
-          : <li key={i} className={classNames({ active: i === 0 })}> <NonMatchView/> </li>
+          : <li
+              key={i}
+              className={classNames({
+                active:
+                  this.state.tabShown ===
+                    marketsData.marketData.markets[key].name || i === 0
+              })}
+            >
+              {" "}<NonMatchView />{" "}
+            </li>
       );
       i++;
     }
